9ffe3c58498a9008e3396688a4e00147d04ded39
Fevronija Rybkina <anton.tsizov@tkvg.ee>
2026-03-08 01:55:04 +0200

    continue

--- prilozhenije.py
+++ prilozhenije.py
@@ -491,7 +491,7 @@ async def insert_boundle_platoky(file:UploadFile = File(...)):
     for i in range(len(nazvanije_platki_vstavka)):
         if nazvanije_platki_vstavka[i] in nazvanije_platki_DB:
             zanjatyje_imena.append(nazvanije_platki_vstavka[i])
-        elif int(id_platki_vstavka[i]) in int(id_platki_DB):
+        elif int(id_platki_vstavka[i]) in id_platki_DB:
             zanjatyje_id.append(id_platki_vstavka[i])
     if len(zanjatyje_id)>0:
         soobhenije1=("Эти артикулы платков уже заняты в БД")

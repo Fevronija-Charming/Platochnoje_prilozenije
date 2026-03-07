9e161bdc62e4755049f4726d6e4e838394fd0594
Fevronija Rybkina <anton.tsizov@tkvg.ee>
2026-03-07 23:50:09 +0200

    exceptions

--- prilozhenije.py
+++ prilozhenije.py
@@ -525,13 +525,7 @@ async def insert_boundle_platoky(file:UploadFile = File(...)):
         platok_s_excel_data["Изображённый_Цветок_5"] = dataframe.iloc[i, 18]
         platok_s_excel_data["Размер_Платка"] = dataframe.iloc[i, 19]
         platok_s_excel_data["Материал_Платка"] = dataframe.iloc[i, 20]
-        platok_s_excel_data["Материал_Бахромы"] = dataframe.iloc[i, 21]
-        try:
-            platok_kontroll = Platok_Schema(**platok_s_excel_data)
-        except:
-            peremycka = (" ")
-            soobhenije = ("Данные не прошли валидацию, ошибка в строке номер") + peremycka + str(i+1)
-            return {"message": soobhenije}
+        platok_kontroll = Platok_Schema(**platok_s_excel_data)
         try:
             session = session_factory()
             platoch_eksemp = Platoky(id=platok_kontroll.id,Название=platok_kontroll.Название_Платка,

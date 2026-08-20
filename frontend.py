ff345c1f0a692ac6724c0ea4202ee0a9fc5d7a73
Fevronija Rybkina <anton.tsizov@tkvg.ee>
2026-08-20 08:11:43 +0300

    ryzov -obrezka str

--- frontend.py
+++ frontend.py
@@ -443,8 +443,8 @@ async def otris_kolority():
                                                                         DisplayLookup(field="Размер",title="Размер",mode=DisplayMode.markdown),
                                                                         DisplayLookup(field="Примечание",title="Примечание",mode=DisplayMode.markdown),
                                                                         DisplayLookup(field="Фотография",title="Фотография",mode=DisplayMode.markdown),
-                                                                         ],),], class_name="fs-5 text-center")
-
+                                                                         ],),], class_name="d-flex flex-column align-items-center fs-6 text-center")
+#class_name="fs-6 text-center"
 @gamajun.get("/api/results", response_model=FastUI,response_model_exclude_none=True)
 async def show_platoky():
     import psycopg2 as ps
